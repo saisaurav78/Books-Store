f4ca0bf63f95596178b99017d2a2211e684c9820
saisaurav <saisaurav78@gmail.com>
2024-11-13 18:06:04 +0530

    updated cors

--- Server/app.js
+++ Server/app.js
@@ -7,13 +7,25 @@ import connectDB from './config/connection.js';
 
 dotenv.config();
 const app = express();
-app.use(cors())
+app.use(
+  cors({
+    origin: [
+      'https://books-store-navy.vercel.app',
+      'https://books-store-git-main-saisaurav78s-projects.vercel.app',
+      'https://books-store-oenr1q48s-saisaurav78s-projects.vercel.app',
+    ],
+    methods:['GET','POST','DELETE']
+  })
+);
 app.use(express.json()); 
 const PORT = process.env.PORT || 3000;
 
 connectDB()
 
 app.use('/books', booksRoute)
+app.get('/', (req, res) => {
+  res.status(200).send('hello from backend')
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}/`);
